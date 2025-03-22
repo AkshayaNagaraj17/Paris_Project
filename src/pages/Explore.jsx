a69779d40848428fa44a86dde8c7cbc748ee445e
AkshayaNagaraj <cnagaraj332@gmail.com>
2025-03-22 12:35:45 +0530

    Fixed line endings

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -3,6 +3,83 @@ function Explore(){
 
     return(
             <>
+            <h1 className="text-6xl font-bold text-center my-10">Explore The City Of <span className="text-red-600">Love</span></h1>
+
+            <iframe className="mx-auto w-3/4 h-150 rounded-xl" width="560" height="315" src="https://www.youtube.com/embed/XC5ssX_RZa0?si=eAAdiDGR-Nbzrebh" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <div className=" border-3 border-gray-300 p-10 m-5 rounded-lg shadow-2xl mt-10">
+            <div className="flex">
+                <img src="./images/e1.jpg" className="w-1/4"alt="" />
+                <div className="mx-30">
+                <h1 className="text-4xl font-bold">Eiffel Tower (Tour Eiffel)</h1>
+                <h2 className="text-2xl mt-5">#1 in Best Things To Do in Paris</h2>
+                </div>
+            </div>
+            <div className="w-3/4 my-10 text-xl">
+                <p>Designed and constructed for the 1889 Exposition Universelle (the World's Fair), the Eiffel Tower was always meant to be a temporary structure, but it has skirted demolition twice. The first time, in 1909, the tower was kept around because of its potential as a transmission tower (an antenna was installed atop the tower). Gustav Eiffel, chief architect of the Eiffel Tower, had a variety of scientific experiments tested on the tower with the hope that any discoveries would help prolong its lifespan. One of these included a wireless transmissions test, which the tower passed with flying colors. During World War I, the Eiffel Tower's transmission capabilities enabled it to intercept communications from enemies as well as relay intel to troops on the ground. The second time the Eiffel Tower was almost destroyed was during the German occupation of France during World War II. Hitler planned to get rid of the tower, but never ended up going through with his plan.</p>
+            </div>
+            </div>
+
+            <div className=" border-3 border-gray-300 p-10 m-5 rounded-lg shadow-2xl mt-10">
+            <div className="flex">
+                <img src="./images/e2.jpg" className="w-1/4"alt="" />
+                <div className="mx-30">
+                <h1 className="text-4xl font-bold">Musée du Louvre</h1>
+                <h2 className="text-2xl mt-5">#2 in Best Things To Do in Paris</h2>
+                </div>
+            </div>
+            <div className="w-3/4 my-10 text-xl">
+                <p>U.S. News Insider Tip: The Louvre is free for all visitors on the first Friday of the month after 6 p.m. (except in July and August), and all day on Bastille Day (July 14). – Laura French 
+
+If you only had time to visit one museum in Paris, it should undoubtedly be the Musée du Louvre. That's because the Louvre is not only widely considered to be one of the best art museums in Europe, but one of the best in the world. The museum first opened its doors in 1793 and features more than 35,000 works of art on display. Here, you can get up close to a variety of art from different time periods and cultures.</p>
+            </div>
+            </div>
+
+
+            <div className=" border-3 border-gray-300 p-10 m-5 rounded-lg shadow-2xl mt-10">
+            <div className="flex">
+                <img src="./images/e3.jpg" className="w-1/4"alt="" />
+                <div className="mx-30">
+                <h1 className="text-4xl font-bold">Seine River</h1>
+                <h2 className="text-2xl mt-5">#3 in Best Things To Do in Paris</h2>
+                </div>
+            </div>
+            <div className="w-3/4 my-10 text-xl">
+                <p>You won’t have much trouble finding the Seine, as it flows directly through the heart of Paris. The river is perhaps one of the most famous waterways in the world and an attraction in itself. It's also useful for more practical reasons: It flows from east to west, dividing the city into the Left Bank and the Right Bank. Knowing where you are in relation to the Seine can help you find your way around during your trip.
+
+For tourists, the waterway mostly serves as a photo backdrop, but it is a lifeline for locals. It's a reliable water supply, a major transportation route and vital for many kinds of commerce. It has also served as a source of sustenance for many fishermen dating back to the third century. In 1991, the Seine River was named a UNESCO World Heritage Site due to its cultural significance in both the past and the present.</p>
+            </div>
+            </div>
+
+
+            <div className=" border-3 border-gray-300 p-10 m-5 rounded-lg shadow-2xl mt-10">
+            <div className="flex">
+                <img src="./images/e4.jpg" className="w-1/4"alt="" />
+                <div className="mx-30">
+                <h1 className="text-4xl font-bold">Musée d'Orsay</h1>
+                <h2 className="text-2xl mt-5">#4 in Best Things To Do in Paris</h2>
+                </div>
+            </div>
+            <div className="w-3/4 my-10 text-xl">
+                <p>U.S. News Insider Tip: Visit on the first Sunday of the month for free entry (when it’s also free to enter the Centre Pompidou, Musée de l'Orangerie, Musée du Rodin, Musée Picasso and several other attractions). – Laura French
+
+Housed in a former railway station along the Left Bank, the Musée d'Orsay is regarded for its rich collection of impressionist works. You'll see paintings by French artists like Degas, Monet, Cezanne and Van Gogh, among many, many others. The museum also houses a number of sculptures, as well as photography and even furniture displays. And if you climb to the museum's top balcony, you can catch a breathtaking view of the Sacré-Coeur Basilica through the museum's massive transparent clock.</p>
+            </div>
+            </div>
+
+
+
+            <div className=" border-3 border-gray-300 p-10 m-5 rounded-lg shadow-2xl mt-10">
+            <div className="flex">
+                <img src="./images/e5.jpg" className="w-1/4"alt="" />
+                <div className="mx-30">
+                <h1 className="text-4xl font-bold">Luxembourg Gardens (Jardin du Luxembourg)</h1>
+                <h2 className="text-2xl mt-5">#5 in Best Things To Do in Paris</h2>
+                </div>
+            </div>
+            <div className="w-3/4 my-10 text-xl">
+                <p>A warm-weather oasis that offers the simplest of pleasures, the Luxembourg Gardens provide ample green space (60 acres) for sun-soaking and people-watching, plus there are plenty of activities to keep kids entertained. When the city bustle becomes too overwhelming, meander around the paths and formal gardens, or just relax with a picnic. Kids can float sailboats at the Grand Basin, ride ponies, take a spin on the merry-go-round, or catch a puppet show at the on-site Theatre des Marionnettes. Adults might delight in the on-site Musée du Luxembourg, the first French museum that was opened to the public. Though with 106 sculptures to its name, including a replica of the Statue of Liberty, the Luxembourg Gardens could easily be considered an open-air museum itself.</p>
+            </div>
+            </div>
             </>
     )
 }
