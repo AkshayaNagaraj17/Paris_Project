de40c40e830580f573780fabbd3fa2de3726b917
AkshayaNagaraj <cnagaraj332@gmail.com>
2025-03-22 12:33:26 +0530

    Fix line endings

---
+++ src/pages/History.jsx
@@ -0,0 +1,58 @@
+function History() {
+  return (
+    <>
+      <div className="relative flex flex-wrap">
+        <img src="./images/his.jpg" className="w-full h-200 " alt="" />
+        <div className="absolute  top-0  my-20 left-1/2 transform -translate-x-1/2 text-black font-serif text-4xl lg:text-6xl font-bold tracking-widest  px-4 py-2">
+          HISTORY OF PARIS
+        </div>
+      </div>
+
+      <div className="mx-auto w-3/4 flex flex-wrap">
+        <h1 className="font-serif text-3xl font-semibold my-10">Foundation</h1>
+
+        <p className="font-sans text-xl my-5">The history of Paris dates back to approximately 259 BC, with the Parisii, a Celtic tribe settled on the banks of the Seine. In 52 BC, the fishermen village was conquered by the Romans, founding a Gallo-Roman town called Lutetia.</p>
+        <p className="font-sans text-xl my-5">The city changed its name to Paris during the fourth century. During this period, the city was threatened by Attila the Hun and his army, and according to the legend, the inhabitants of Paris resisted the attacks thanks to the providential intervention of Saint Geneviève (patron saint of the city).</p>
+        <p className="font-sans text-xl my-5">In 508 the first king of the Franks, Clovis I, made Paris the capital of his empire. In 987, the Capetian dynasty came to power until 1328.
+
+During the eleventh century, Paris gradually became more prosper thanks to its trade in silver and because it was a strategic route for pilgrims and traders.</p>
+
+<img src="./images/h1.jpg" className="mx-auto mt-10 w-1/2 rounded-4xl" alt="" />
+
+      </div>
+      <p className=" text-2xl font-semibold text-center mt-5 ">Old Metro Sign Paris</p>
+   
+      <div className="mx-auto w-3/4 flex flex-wrap">
+        <h1 className="font-serif text-3xl font-semibold my-10">Urban development</h1>
+
+        <p className="font-sans text-xl my-5">Once Napoleon had been defeated, France experienced great political uncertainty until Napoleon’s nephew organized a coup d’état in 1851 and became Emperor Napoleon III. During the following seventeen years, Napoleon III promoted the city’s urban development.</p>
+        <p className="font-sans text-xl my-5">During this period and with Baron Haussmann as the prefect of Paris, the city changed its urban structure, rebuilding the center, knocking down its fortification and expanding the metropolitan territory.</p>
+        <p className="font-sans text-xl my-5">On the 28 January 1871, Paris was conquered by the Prussian troops and a few years later (at the end of 1800), the Third Republic was proclaimed. With the new government, an era of economic growth began for the city, promoting in 1889 the construction of the Eiffel Tower, worldwide symbol of Paris.
+
+During the eleventh century, Paris gradually became more prosper thanks to its trade in silver and because it was a strategic route for pilgrims and traders.</p>
+
+<img src="./images/h2.jpg" className="mx-auto mt-10 w-1/2 rounded-4xl" alt="" />
+
+      </div>
+      <div className="mx-auto w-3/4 flex flex-wrap mb-20">
+        <h1 className="font-serif text-3xl font-semibold my-10">Contemporary Paris</h1>
+
+        <p className="font-sans text-xl my-5">From the twentieth century on, Paris suffered important changes with the reconstruction of different neighborhoods, many damaged during World War I and World War II.</p>
+        <p className="font-sans text-xl my-5">During World War I, the city resisted the German offensives. However, in 1940, Paris was occupied by the Nazis, although the Parisians resisted and freed the capital on 25 August 1944.
+
+During the war against Algeria, several violent manifestations took place in Paris against the war, with numerous attacks by the OAS (Organisation of the Secret Army).</p>
+        <p className="font-sans text-xl my-5">One of the last riots to take place in Paris was in March 2006, when students poured out onto the streets and protested against the labour market reform.
+
+In November 2015, Paris witnessed a tragic event, several terrorist attacks hit the city and the suburbs of Saint-Denis, killing 137 people and injuring 415. </p>
+
+
+      </div>
+      
+
+      
+
+    </>
+  );
+}
+
+export default History;
